da0b03e4d74d9340fb0b9f71ecb1e26a37dc0fa5
Albert Sh <at271989@gmail.com>
2024-10-09 09:32:26 +0300

    Урок #9 - Форма для добавления записей в БД

--- AsutpWebsite/MyWebSite/news/models.py
+++ AsutpWebsite/MyWebSite/news/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class Articles(models.Model):
-    title = models.CharField('Название',max_length=50, default='Пусто')
-    anons = models.CharField('Анонс',max_length=250, default='Пусто')
+    title = models.CharField('Название',  max_length=50)
+    anons = models.CharField('Анонс', max_length=250)
     full_text = models.TextField('Статья')
     date = models.DateTimeField('Дата публикации')
 
